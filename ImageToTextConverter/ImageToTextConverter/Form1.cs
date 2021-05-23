4fc1a839bda7bcbf6ac74eb53441d6b48d6ee26c
Ranjota <36625861+Ranjota@users.noreply.github.com>
2021-05-23 10:33:40 +0530

    Update Form1.cs

--- ImageToTextConverter/ImageToTextConverter/Form1.cs
+++ ImageToTextConverter/ImageToTextConverter/Form1.cs
@@ -17,7 +17,7 @@ namespace ImageToTextConverter
     {
         // To store multiple file names
         //Hi
-        //Heya!!
+        //Heya!! How are you??
         public List<string> filePaths = new List<string>();
         public Form1()
         {
